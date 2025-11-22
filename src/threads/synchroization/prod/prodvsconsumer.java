0c61a3462428a388c7348d0739e37de1a9e3b3ef
Rohit Prashar <rohit.prashar1@hmcts.net>
2025-11-22 20:10:10 +0000

    ai changes

---
+++ src/threads/synchroization/prod/prodvsconsumer.java
@@ -0,0 +1,14 @@
+package threads.synchroization.prod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class prodvsconsumer {
+
+
+    // i have a list in which i will use a thread to put itmes and a consumer to fetch items from that
+
+    public static void main(String[] args) {
+    }
+}
